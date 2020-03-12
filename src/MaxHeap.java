18f893f9e214e6374380ea7b6af36821b6a4e2a7
Srisurya Chandramouli <csrisurya@gmail.com>
2020-03-12 14:21:47 -0700

    worked on heapifyDown, MaxHeapN, and pop

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -21,16 +21,27 @@ public class MaxHeap implements Heap {
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
+        this.data = data;
+        int index = this.data.length-1;
+        for(int i = index; i > 0; i--){
+            heapifyUp(i);
+            if(leftChildIndex(i) < data.length && data[leftChildIndex(i)] > data[i]){
+                heapifyUp(leftChildIndex(i));
+            }
+            if(rightChildIndex(i) < data.length && data[rightChildIndex(i)] > data[i]){
+                heapifyUp(rightChildIndex(i));
+            }
+        }
 
     }
 
-    private int parent(int index) {
+    private int parentIndex(int index) {
         return (index-1)/2;
     }
-    private int leftChild(int index){
+    private int leftChildIndex(int index){
         return (index*2) + 1;
     }
-    private int rightChild(int index){
+    private int rightChildIndex(int index){
         return (index*2) + 2;
     }
     private void swap(int index1, int index2){
@@ -39,13 +50,13 @@ public class MaxHeap implements Heap {
         data[index2] = temp;
     }
     public void heapifyUp(int index){
-        if(index != 0 && data[index] > data[parent(index)]){
-            swap(index, parent(index));
-            heapifyUp(parent(index));
+        if(index != 0 && data[index] > data[parentIndex(index)]){
+            swap(index, parentIndex(index));
+            heapifyUp(parentIndex(index));
         }
     }
     // add an item to the heap
-    public boolean add(Integer item) { 
+    public boolean add(Integer item) {
         data[size] = item;
         heapifyUp(size);
         size++;
@@ -60,24 +71,25 @@ public class MaxHeap implements Heap {
     // remove the root item
     public void heapifyDown(int index){ // work on this
         int smalllestIndex = index;
-        int leftIndex = leftChild(index);
-        int rightIndex = rightChild(index);
-        if(leftIndex <= index && data[leftIndex] < data[smalllestIndex]){
-            smalllestIndex = leftIndex;
+
+        if(leftChildIndex(index) < size && data[leftChildIndex(index)] > data[index]){
+            smalllestIndex = leftChildIndex(index);
         }
-        if(rightIndex <= index && data[rightIndex] < data[smalllestIndex]){
-            smalllestIndex = rightIndex;
+        if(rightChildIndex(index) < size && data[rightChildIndex(index)] > data[leftChildIndex(index)]){
+            smalllestIndex = rightChildIndex(index);
         }
+
         if(smalllestIndex != index){
+            swap(smalllestIndex, index);
             heapifyDown(smalllestIndex);
         }
     }
     public Integer pop() { // work on this
         int root = data[0];
-        swap(root, size-1);
+        swap(0, size-1);
         data[size-1] = null;
         size--;
-        heapifyDown(size);
+        heapifyDown(0);
         return root;
     }
 
@@ -86,6 +98,13 @@ public class MaxHeap implements Heap {
         Integer[] array = {2,5,6,8,4};
         heap.MaxHeapLogN(array);
         System.out.println(Arrays.toString(heap.data));
+//        heap.pop();
+//        System.out.println(Arrays.toString(heap.data));
+
+        MaxHeap heap2 = new MaxHeap(5);
+        heap2.MaxHeapN(array);
+        System.out.println(Arrays.toString(heap2.data));
+
 
 
     }
